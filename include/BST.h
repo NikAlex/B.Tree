22ab4a8f27a51f06fdff4cb634948c8397b964d0
NikAlex <alez90@yandex.ru>
2016-05-18 20:38:14 +0400

    Update BST.h

--- include/BST.h
+++ include/BST.h
@@ -1,83 +1,83 @@
-#ifndef BST_h
-#define BST_h
 #pragma once
-#include <iostream>
+#include <algorithm>
+#include <cstdlib>
 #include <fstream>
-using namespace std;
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <stdexcept>
+#include <vector>
 
-template <class T>
-class BST;
+using std::exception;
+using std::ostream;
+using std::istream;
+using std::fstream;
+using std::vector;
+using std::unique_ptr;
+using std::iterator;
+using std::output_iterator_tag;
+using std::cout;
 
-template <class T>
-ostream & operator<<(ostream & out, BST<T> &tree);
+#ifndef BT_H
+#define BT_H
 
-template <class T>
-ifstream & operator >> (ifstream & fin, BST<T> &tree);
+template <typename T>
+class BinarySearchTree;
 
-template <class T>
-ofstream & operator<<(ofstream & fout, BST<T> &tree);
+template <typename T>
+ostream & operator<<(ostream & output, BinarySearchTree<T> &);
+template <typename T>
+istream & operator >> (istream & input, BinarySearchTree<T> &);
+template <typename T>
+fstream & operator<<(fstream & file, BinarySearchTree<T> &);
+template <typename T>
+fstream & operator >> (fstream & file, BinarySearchTree<T> &);
 
-class Exceptions {
-	char* err;
-public:
-	Exceptions(char* _err);
-	char* what();
+template <typename T>
+struct node {
+	typedef unique_ptr<node<T>> ptrNode;
+
+	T			data;
+	ptrNode		left;
+	ptrNode		right;
+	node(const T & value) : data(value), left(nullptr), right(nullptr) {};
 };
 
-class File_Not_Open : public Exceptions {
-public:
-	File_Not_Open();
-};
-
-class Busy : public Exceptions {
-public:
-	Busy();
-};
-
-class Empty_tree : public Exceptions {
-public:
-	Empty_tree();
-};
-
-class Element_not_found : public Exceptions{  
-public:
-	Element_not_found();
-};
-
-class Tree_Was_Deleted : public Exceptions{  
-public:
-	Tree_Was_Deleted();
-};
-
-template <class T>
-class BST {
-public:
-	BST();
-	bool add(T x);
-	bool search(T x);
-	friend ostream & operator<< <>(ostream &out, BST<T> &tree);
-	friend ifstream & operator>> <>(ifstream &fin, BST<T> &tree);
-	friend ofstream & operator<< <>(ofstream &fout, BST<T> &tree);
-	bool del(T x);
-	void Create_tree_again();
+template <typename T>
+class BinarySearchTree {
 private:
-	class Node;
-	Node* parent;
+	typedef		unique_ptr<node<T>> ptrNode;
+	ptrNode		root;
+	size_t		count;
+	size_t		existed;
+	vector<T>	elements;
+
+	auto		print(const unique_ptr<node<T>>&, ostream & os = cout, size_t width = 0)->void;
+	auto		remove(const T&, node<T>*)->node<T> *;
+	auto		findMin(unique_ptr<node<T>>&)->node<T> *;
+	auto		search(const T&, node<T>*)->node<T> *;
+	auto		createVector(const unique_ptr<node<T>> &m_node, vector<T> &elem)->vector<T>;
+public:
+	BinarySearchTree() : root(nullptr), count(0), existed(0) {};					// TESTED
+	BinarySearchTree(size_t k) : root(nullptr), count(k), existed(0) {};			// TESTED
+	BinarySearchTree(const std::initializer_list<T> &list);							// WORKS
+
+	auto		insert(const T&)->bool; 									// TESTED
+	auto		remove(const T&)->node<T> *;								// tested?..
+	auto		search(const T&)->node<T> *;								// TESTED
+	auto		getCount() const->size_t;									// TESTED
+	auto		getNumber() const->size_t;
+	auto		createVector()->vector<T>;
+	auto		getRoot()->T;
+	auto		begin();
+	auto		end();
+
+	friend		istream & operator >> <>(istream &, BinarySearchTree &);		// TESTED
+	friend		fstream & operator >> <>(fstream &, BinarySearchTree &);		// TESTED
+	friend		ostream & operator<< <>(ostream &, BinarySearchTree &);		// TESTED
+	friend		fstream & operator<< <>(fstream &, BinarySearchTree &);		// TESTED
 };
 
-template <class T>
-class BST<T>::Node {
-public:
-	Node(T x);
-	void add(T x);
-	bool search(T x);
-	bool print_console();
-	void del(T x);
-	T min(Node* M);
-	bool print_file(ofstream &fout);
-private:
-	T k;
-	Node *l;
-	Node *r;
-};
+#include "BinarySearchTree.cpp"
+
 #endif
