4726a04e427607d916e0174b8b0ae8f3498da0de
NikAlex <alez90@yandex.ru>
2016-05-19 02:07:27 +0400

    Update BST.cpp

--- include/BST.cpp
+++ include/BST.cpp
@@ -1,235 +1,166 @@
-#include "BST.h"
+#include "binarysearchtree.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+Iscluchenia::Iscluchenia(char* _err) : err(_err){}
+char* Iscluchenia::what() { return err; }
+Uzhe_est::Uzhe_est() : Iscluchenia("ERROR: etot element uzhe dobavlen!") {}
+File_Not_Open::File_Not_Open() : Iscluchenia("ERROR: file not open!") {}
+Pustoe_derevo::Pustoe_derevo() : Iscluchenia("ERROR: derevo pusto!") {}
+Element_not_found::Element_not_found() : Iscluchenia("ERROR: takogo elementa v dereve net!") {}
+Tree_Was_Deleted::Tree_Was_Deleted() : Iscluchenia("ERROR: derevo udaleno!") {}
 
-using std::unique_ptr;
-using std::move;
-using std::ostream;
-using std::istream;
-using std::fstream;
-using std::endl;
-
-#ifndef BT_CPP
-#define BT_CPP
-
-template <typename T>	// WORKS
-auto BinarySearchTree<T>::insert(const T &ref) -> bool {
-    this->existed += 1;
-    unique_ptr<node<T>> m_node(new node<T>(ref));
-
-    if (root == nullptr) {
-        root = move(m_node);
-        return true;
-    }
-    else {
-        node<T>* temp = root.get();
-        node<T>* prev = root.get();
-
-        while (temp != nullptr) {
-            prev = temp;
-            if (temp->data < ref) {
-                temp = temp->right.get();
-            }
-            else if (temp->data == ref) {
-                throw std::logic_error("Already exist");
-            }
-            else {
-                temp = temp->left.get();
-            }
-        }
-
-        if (prev->data < ref) {
-            prev->right = move(m_node);
-        }
-        else {
-            prev->left = move(m_node);
-        }
-        return true;
-    }
-    return false;
+template <class Z>
+BinarySearchTree<Z>::der::der(Z x) : D(x), l(nullptr), r(nullptr){}
+template <class Z>
+void BinarySearchTree<Z>::der::do_free(){
+	f:
+	if (this){
+		if (l){
+			if ((l->l) || (l->r)) l->do_free();
+			else { delete l; l = nullptr; }
+		}
+		if (r){
+			if ((r->l) || (r->r)) r->do_free();
+			else { delete r;  r = nullptr; }
+		}
+		if (l || r) goto f;
+	}
+}
+template <class Z>
+void BinarySearchTree<Z>::der::add(Z x){
+	if (x < D){
+		if (l != nullptr) l->add(x);
+		if (l == nullptr) l = new der(x);
+	}
+	if (x>D){
+		if (r != nullptr) r->add(x);
+		if (r == nullptr) r = new der(x);
+	}
+}
+template <class Z>
+bool BinarySearchTree<Z>::der::search(Z x){
+	if (x == D) { return true; }
+	if (x > D) if (r != nullptr) return(r->search(x));
+	if (x < D) if (l != nullptr) return(l->search(x));
+	return false;
+}
+template <class Z>
+Z BinarySearchTree<Z>::der::min(der* G){
+	if (G->l) return min(G->l);
+	else return G->D;
+}
+template <class Z>
+void BinarySearchTree<Z>::der::del(Z x){
+	if ((x == D) && (!l) && (!r)) { delete this; throw Tree_Was_Deleted(); }
+	if ((x == D) && (!l)) {
+		D = r->D;
+		if (r->l) l = r->l; else { delete l; l = nullptr; }
+		if (r->r) r = r->r; else { delete r; r = nullptr; }
+		return;
+	}
+	if ((x == D) && (!r)) {
+		D = l->D;
+		if (l->r) r = l->r; else { delete r; r = nullptr; }
+		if (l->l) l = l->l; else { delete l; l = nullptr; }
+		return;
+	}
+	if (x < D) {
+		if ((l->D == x) && (!(l->r)) && (!(l->l))) { delete l; l = nullptr; return; }
+		if ((l->D == x) && (l->l) && (l->r)) { l->D = min(l->r); if (l->r->D != min(l->r)) l->r->del(min(l->r)); else { delete l->r; l->r = nullptr; } return; }
+		else; l->del(x);
+		return;
+	}
+	if (x > D) {
+		if ((r->D == x) && (!(r->r)) && (!(r->l))) { delete r; r = nullptr; return; }
+		if ((r->D == x) && (r->l) && (r->r)) { r->D = min(r->r); if (r->r->D != min(r->r)) r->r->del(min(r->r)); else { delete r->r; r->r = nullptr; } return;}
+		else r->del(x);
+		return;
+	}
+	if ((x == D) && (l) && (r)) { D = min(r); if (r->D != min(r)) r->del(min(r)); else { delete r; r = nullptr; } return; }
 }
 
-template <typename T>	// WORKS
-auto BinarySearchTree<T>::remove(const T &key) -> node<T> * {
-    if (search(key) == nullptr) {
-        throw std::invalid_argument("Wrong index");
-    }
-    node<T> * leaf = root.get();
-    return remove(key, leaf);
+
+template <class Z>
+void BinarySearchTree<Z>::sozdaem_derevo_snova() {
+	root = nullptr;
 }
 
-template<typename T>	// HARD, BUT WORKS
-auto BinarySearchTree<T>::remove(const T &key, node<T> * leaf) -> node<T> * {
-    // C - crutches
-    if (leaf == nullptr) {
-        return leaf;
-    }
-
-    if ((leaf->left.get() != nullptr) && ((leaf->left.get())->data == key)) {
-        if (((leaf->left.get())->left.get() == nullptr) && ((leaf->left.get())->right.get() == nullptr)) {
-            leaf->left.reset();
-            this->existed -= 1;
-            return leaf;
-        }
-    }
-    else if ((leaf->right.get() != nullptr) && ((leaf->right.get())->data == key)) {
-        if (((leaf->right.get())->left.get() == nullptr) && ((leaf->right.get())->right.get() == nullptr)) {
-            leaf->right.reset();
-            this->existed -= 1;
-            return leaf;
-        }
-    }
-    if (key < leaf->data) {
-        leaf = remove(key, leaf->left.get());
-    }
-    else if (key > leaf->data) {
-        leaf = remove(key, leaf->right.get());
-    }
-    else if ((leaf->left.get() != nullptr) && (leaf->right.get() != nullptr)) {
-        leaf->data = findMin(leaf->right)->data;
-        this->existed -= 1;
-        return leaf;
-    }
-    else {
-        if (leaf->left.get() != nullptr) {
-            leaf->data = (leaf->left.get())->data;
-            leaf->left.reset();
-            this->existed -= 1;
-            return leaf;
-        }
-        else {
-            leaf->data = (leaf->right.get())->data;
-            leaf->right = move((leaf->right.get())->right);
-            this->existed -= 1;
-            return leaf;
-        }
-    }
-    return leaf;
+template <class Z>
+bool BinarySearchTree<Z>::der::print_console(){
+	if (this != nullptr){
+		if (l != nullptr) l->print_console();
+		cout << D << " ";
+		if (r != nullptr) r->print_console();
+		return true;
+	}
+	else return false;
+}
+template <class Z>
+bool BinarySearchTree<Z>::der::print_file(ofstream &fout){
+	if (this != nullptr){
+		if (fout.is_open()){
+			if (l != nullptr) l->print_file(fout);
+			fout << D << " ";
+			if (r != nullptr) r->print_file(fout);
+		}
+		return true;
+	}
+	return false;
 }
 
-template<typename T>	// Works?.. Hmm...
-auto BinarySearchTree<T>::findMin(unique_ptr<node<T>>& leaf) -> node<T> * {
-    if (leaf->left.get() == nullptr) {
-        node<T> *tmp = new node<T>(leaf.get()->data);
-        if (((leaf.get())->right).get() == nullptr) {
-            leaf.reset();
-        }
-        else {
-            leaf = move((leaf.get()->right));
-        }
-        return tmp;
-    }
-    return findMin(leaf->left);
+template <class Z>
+BinarySearchTree<Z>::BinarySearchTree() : root(nullptr){}
+template <class Z>
+BinarySearchTree<Z>::BinarySearchTree(initializer_list<Z> L){
+	root = nullptr;
+	for (int i : L)
+	{
+		add(i);
+	}
 }
-
-template <typename T>	// WORKS
-auto BinarySearchTree<T>::search(const T &key) -> node<T> * {
-    node<T>* leaf = root.get();
-    return search(key, leaf);
+template <class Z>
+BinarySearchTree<Z>::~BinarySearchTree() {
+	if(root) {root->do_free(); delete root;}
 }
-
-template <typename T>	// WORKS
-auto BinarySearchTree<T>::search(const T & key, node<T>* leaf) -> node<T> * {
-    if (leaf != nullptr) {
-        if (key == leaf->data) {
-            return leaf;
-        }
-        if (key < leaf->data) {
-            return search(key, leaf->left.get());
-        }
-        else {
-            return search(key, leaf->right.get());
-        }
-    }
-    else {
-        throw std::invalid_argument("Wrong index");
-    }
+template <class Z>
+bool BinarySearchTree<Z>::add(Z x){
+	if (root != nullptr) if (search(x)) throw Uzhe_est();
+	if (root == nullptr) { root = new der(x); return true; }
+	else { root->add(x); return true; }
+	return false;
 }
-
-template <typename T>	// WORKS
-auto BinarySearchTree<T>::getCount() const -> size_t {
-    return this->count;
+template <class Z>
+bool BinarySearchTree<Z>::search(Z x){
+	if (root == nullptr) throw Pustoe_derevo();
+	return(root->search(x));
 }
-
-template<typename T>	// WORKS
-auto BinarySearchTree<T>::getNumber() const -> size_t {
-    return this->existed;
+template <class Z>
+bool BinarySearchTree<Z>::del(Z x){
+	if (root == nullptr) throw Pustoe_derevo();
+	if (!this->search(x)) throw Element_not_found();
+	try{ root->del(x); }
+	catch (Tree_Was_Deleted &){ throw Tree_Was_Deleted(); }
+	return true;
 }
-
-template <typename T>	// WORKS
-auto BinarySearchTree<T>::print(const unique_ptr<node<T>> &m_node, ostream & os, size_t width) -> void {
-    node<T>* tmp = m_node.get();
-
-    if (m_node.get() == nullptr) {
-        return;
-    }
-    if (tmp->left.get() != nullptr) {
-        print(tmp->left, os, width + 3);
-    }
-    if (width) {
-        os.width(width);
-        os << ' ';
-    }
-    os << m_node->data << endl;
-    if (tmp->right.get()) {
-        print(tmp->right, os, width + 3);
-    }
+template <class Z>
+ostream & operator <<(ostream & out, BinarySearchTree<Z> & tree) {
+	if (tree.root->print_console()) return out;
+	else throw Pustoe_derevo();
 }
-
-template <typename T>	// WORKS
-ostream & operator <<(ostream & os, BinarySearchTree<T> & x) {
-    if (x.root == nullptr) {
-        throw std::logic_error("Empty tree");
-    }
-    x.print(x.root, os);
-
-    return os;
+template <class Z>
+ifstream& operator >>(ifstream & fin, BinarySearchTree<Z> & tree) {
+	if (!fin.is_open()) throw File_Not_Open();
+	Z x;
+	while (!fin.eof()){
+		if (fin>>x) tree.add(x);
+		else break;
+	}
+	return fin;
 }
-
-template <typename T>	// WORKS
-istream & operator >>(istream & input, BinarySearchTree<T> & x) {
-    T temp;
-    if (x.count == 0) {
-        throw std::logic_error("Empty tree");
-    }
-    else {
-        for (size_t i = 0; i < x.count; ++i) {
-            if (input >> temp) {
-                x.insert(temp);
-            }
-            else {
-                throw std::logic_error("Error in input stream");
-            }
-        }
-        return input;
-    }
+template <class Z>
+ofstream & operator <<(ofstream & fout, BinarySearchTree<Z> & tree) {
+	if (tree.root->print_file(fout)) return fout;
+	else throw Pustoe_derevo();
 }
-
-template <typename T>	//WORKS
-fstream & operator <<(fstream &file, BinarySearchTree<T> & x) {
-    if (x.count == 0) {
-        throw std::logic_error("Empty tree");
-    }
-    x.print(x.root, file);
-
-    return file;
-}
-
-template <typename T>	// WORKS
-fstream & operator >>(fstream &file, BinarySearchTree<T> & x) {
-    T temp;
-    if (x.count == 0) {
-        throw std::logic_error("Empty tree");
-    }
-    else {
-        for (size_t i = 0; i < x.count; ++i) {
-            if (file >> temp) {
-                x.insert(temp);
-            }
-            else {
-                throw std::logic_error("Error in input stream");
-            }
-        }
-        return file;
-    }
-}
-#endif
 
